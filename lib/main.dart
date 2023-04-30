991b09b9b61c2ab781c20a74ea8899515b74c6df
JuniorAlvarenga <127621943+JuniorAlvarenga@users.noreply.github.com>
2023-04-29 18:51:22 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:myapp/opciones.dart';
+import 'opciones.dart';
 
 void main() async {
   await GetStorage.init();
-  runApp(MyApp()); //
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Builder(builder: (context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Bienvenido'),
-          ),
-          body: Center(
-            child: ElevatedButton(
-              child: const Text('Empecemos'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Opciones()),
-                );
-              },
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                'https://c0.wallpaperflare.com/preview/241/384/859/analysis-analytics-analyzing-annual.jpg',
+              ),
+              fit: BoxFit.cover,
             ),
           ),
-        );
-      }),
+          child: Builder(builder: (context) {
+            return Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(175, 75),
+                  backgroundColor: Colors.black,
+                ),
+                child: Text(
+                  'Empecemos',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 30,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Opciones()),
+                  );
+                },
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 }
-
